076840f65fa6edbfe6d2b54e7be78c98bcfd6ebb
santiago <stenahe@gmail.com>
2026-06-23 11:16:47 +0200

    refactor: order file

--- main.go
+++ main.go
@@ -51,10 +51,11 @@ func main() {
 	)
 
 	mux.HandleFunc("POST /api/users", cfg.createUserHandler)
-	mux.HandleFunc("POST /api/validate_chirp", validateChirp)
-	mux.HandleFunc("GET /api/healthz", readinessHandler)
-	mux.HandleFunc("POST /admin/reset", cfg.resetBackEndHandler)
+	mux.HandleFunc("GET /api/chirps", cfg.getChirps)
+	mux.HandleFunc("POST /api/chirps", cfg.validateChirp)
 	mux.HandleFunc("GET /admin/metrics", cfg.readMetricsHandler)
+	mux.HandleFunc("POST /admin/reset", cfg.resetBackEndHandler)
+	mux.HandleFunc("GET /api/healthz", readinessHandler)
 
 	server := &http.Server{
 		Handler: mux,
@@ -62,6 +63,62 @@ func main() {
 	}
 
 	log.Fatal(server.ListenAndServe())
+}
+
+func middlewareLog(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s %s", r.Method, r.URL.Path)
+		next.ServeHTTP(w, r)
+	})
+}
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+	database       *database.Queries
+	platform       string
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) createUserHandler(w http.ResponseWriter, r *http.Request) {
+	type User struct {
+		Email string `json:"email"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	u := User{}
+	err := decoder.Decode(&u)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Something went wrong")
+		return
+	}
+
+	type Payload struct {
+		Id        uuid.UUID `json:"id"`
+		CreatedAt time.Time `json:"created_at"`
+		UpdatedAt time.Time `json:"updated_at"`
+		Email     string    `json:"email"`
+	}
+
+	email := u.Email
+
+	newUser, err := cfg.database.CreateUser(r.Context(), email)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Could not create user")
+		return
+	}
+
+	payload := &Payload{Id: newUser.ID, CreatedAt: newUser.CreatedAt, UpdatedAt: newUser.UpdatedAt, Email: newUser.Email}
+	respondWithJSON(w, http.StatusCreated, payload)
+}
+
+func (cfg *apiConfig) getChirps(w http.ResponseWriter, r *http.Request) {
+
 }
 
 func (cfg *apiConfig) validateChirp(w http.ResponseWriter, r *http.Request) {
@@ -85,7 +142,7 @@ func (cfg *apiConfig) validateChirp(w http.ResponseWriter, r *http.Request) {
 	cleanedBody := censorProfanity(chirp.Body)
 
 	type Payload struct {
-		ID        uuid.UUID `json:"id"`
+		Id        uuid.UUID `json:"id"`
 		CreatedAt time.Time `json:"created_at"`
 		UpdatedAt time.Time `json:"updated_at"`
 		Body      string    `json:"body"`
@@ -99,13 +156,43 @@ func (cfg *apiConfig) validateChirp(w http.ResponseWriter, r *http.Request) {
 	}
 
 	payload := &Payload{
-		ID:        newChirp.ID,
+		Id:        newChirp.ID,
 		CreatedAt: newChirp.CreatedAt,
 		UpdatedAt: newChirp.UpdatedAt,
 		Body:      cleanedBody,
 		UserId:    chirp.UserId,
 	}
 	respondWithJSON(w, http.StatusCreated, payload)
+}
+
+func (cfg *apiConfig) readMetricsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(http.StatusOK)
+	htmlTemplate := `<html>
+	<body>
+	<h1>Welcome, Chirpy Admin</h1>
+	<p>Chirpy has been visited %d times!</p>
+	</body>
+	</html>`
+	message := fmt.Sprintf(htmlTemplate, cfg.fileserverHits.Load())
+	_, err := w.Write([]byte(message))
+	if err != nil {
+		return
+	}
+}
+
+func (cfg *apiConfig) resetBackEndHandler(w http.ResponseWriter, r *http.Request) {
+	if cfg.platform != "dev" {
+		w.WriteHeader(http.StatusForbidden)
+		return
+	}
+	err := cfg.database.DeleteUser(r.Context())
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Could not delete users")
+		return
+	}
+	cfg.fileserverHits.Store(0)
+	w.WriteHeader(http.StatusOK)
 }
 
 func censorProfanity(chirp string) string {
@@ -173,86 +260,4 @@ func readinessHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		return
 	}
-}
-
-func middlewareLog(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("%s %s", r.Method, r.URL.Path)
-		next.ServeHTTP(w, r)
-	})
-}
-
-type apiConfig struct {
-	fileserverHits atomic.Int32
-	database       *database.Queries
-	platform       string
-}
-
-func (cfg *apiConfig) createUserHandler(w http.ResponseWriter, r *http.Request) {
-	type User struct {
-		Email string `json:"email"`
-	}
-
-	decoder := json.NewDecoder(r.Body)
-	u := User{}
-	err := decoder.Decode(&u)
-	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, "Something went wrong")
-		return
-	}
-
-	type Payload struct {
-		Id        uuid.UUID `json:"id"`
-		CreatedAt time.Time `json:"created_at"`
-		UpdatedAt time.Time `json:"updated_at"`
-		Email     string    `json:"email"`
-	}
-
-	email := u.Email
-
-	newUser, err := cfg.database.CreateUser(r.Context(), email)
-	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, "Could not create user")
-		return
-	}
-
-	payload := &Payload{Id: newUser.ID, CreatedAt: newUser.CreatedAt, UpdatedAt: newUser.UpdatedAt, Email: newUser.Email}
-	respondWithJSON(w, http.StatusCreated, payload)
-}
-
-func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		cfg.fileserverHits.Add(1)
-		next.ServeHTTP(w, r)
-	})
-}
-
-func (cfg *apiConfig) readMetricsHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	w.WriteHeader(http.StatusOK)
-	htmlTemplate := `<html>
-	<body>
-	<h1>Welcome, Chirpy Admin</h1>
-	<p>Chirpy has been visited %d times!</p>
-	</body>
-	</html>`
-	message := fmt.Sprintf(htmlTemplate, cfg.fileserverHits.Load())
-	_, err := w.Write([]byte(message))
-	if err != nil {
-		return
-	}
-}
-
-func (cfg *apiConfig) resetBackEndHandler(w http.ResponseWriter, r *http.Request) {
-	if cfg.platform != "dev" {
-		w.WriteHeader(http.StatusForbidden)
-		return
-	}
-	err := cfg.database.DeleteUser(r.Context())
-	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, "Could not delete users")
-		return
-	}
-	cfg.fileserverHits.Store(0)
-	w.WriteHeader(http.StatusOK)
 }
