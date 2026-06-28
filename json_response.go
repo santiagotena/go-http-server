36d121a224a921f1addc91f129d41e9e90ae108a
santiago <stenahe@gmail.com>
2026-06-28 11:01:07 +0200

    feature: add error log

--- json_response.go
+++ json_response.go
@@ -33,6 +33,7 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	w.WriteHeader(code)
 	_, err = w.Write(dat)
 	if err != nil {
+		log.Printf("Error writing the response's body: %s", err)
 		return
 	}
 }
